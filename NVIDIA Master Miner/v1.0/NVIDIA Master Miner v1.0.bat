151ec7f7ac9f6ee87d34809261e4a9a38135faf5
Wasym Atieh Alonso <72618577+iWas-Coder@users.noreply.github.com>
2021-10-21 19:43:30 +0200

    Update NVIDIA Master Miner v1.0.bat

--- NVIDIA Master Miner/v1.0/NVIDIA Master Miner v1.0.bat
+++ NVIDIA Master Miner/v1.0/NVIDIA Master Miner v1.0.bat
@@ -6,8 +6,13 @@
 :: https://github.com/iWas-Coder/wasymatieh
 
 
-echo off
-cls
+:: START of Copyright License
+echo NVIDIA Master Miner [Version v1.0]
+echo (c) iWas-Coder. All rights reserved.
+@timeout /t 3 /nobreak
+:: END of Copyright License
+
+
 
 :master_menu
 	echo off
