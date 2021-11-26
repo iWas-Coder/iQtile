d284300a9df589149b509b4d8c778654c7d885b7
Wasym Atieh Alonso <72618577+iWas-Coder@users.noreply.github.com>
2021-11-26 16:18:29 +0100

    Update zsh_config.sh

--- scripts/zsh_config.sh
+++ scripts/zsh_config.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
+echo -e "███████╗███████╗██╗  ██╗     ██████╗ ██████╗ ███╗   ██╗███████╗██╗ ██████╗ "
+echo -e "╚══███╔╝██╔════╝██║  ██║    ██╔════╝██╔═══██╗████╗  ██║██╔════╝██║██╔════╝ "
+echo -e "  ███╔╝ ███████╗███████║    ██║     ██║   ██║██╔██╗ ██║█████╗  ██║██║  ███╗"
+echo -e " ███╔╝  ╚════██║██╔══██║    ██║     ██║   ██║██║╚██╗██║██╔══╝  ██║██║   ██║"
+echo -e "███████╗███████║██║  ██║    ╚██████╗╚██████╔╝██║ ╚████║██║     ██║╚██████╔╝"
+echo -e "╚══════╝╚══════╝╚═╝  ╚═╝     ╚═════╝ ╚═════╝ ╚═╝  ╚═══╝╚═╝     ╚═╝ ╚═════╝ "
+
 if [[ $(/usr/bin/id -u) -ne 0 ]]; then
     echo "Not running as root. Please run this script with sudo:"
     echo "sudo $0 $*"
     exit 1
 fi
 
-curl -L https://github.com/iWas-Coder/iQtile/blob/main/dotfiles/zsh_user.zip > zsh_user.zip
-curl -L https://github.com/iWas-Coder/iQtile/blob/main/dotfiles/zsh_root.zip > zsh_root.zip
+curl -L https://github.com/iWas-Coder/iQtile/raw/main/dotfiles/zsh_user.zip > zsh_user.zip
+curl -L https://github.com/iWas-Coder/iQtile/raw/main/dotfiles/zsh_root.zip > zsh_root.zip
 unzip zsh_user.zip
 rm zsh_user.zip
 mv zsh_user/.zsh/ zsh_user/powerlevel10k/ zsh_user/.p10k.zsh zsh_user/.zshrc /home/$USER/ && rmdir zsh_user/
