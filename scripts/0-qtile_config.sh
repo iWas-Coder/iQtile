209abaad4ee2b0bf3f58e37687146f6e3ad3b3d8
Wasym Atieh Alonso <72618577+iWas-Coder@users.noreply.github.com>
2021-11-26 15:38:46 +0100

    Update 0-qtile_config.sh

--- scripts/0-qtile_config.sh
+++ scripts/0-qtile_config.sh
@@ -5,7 +5,7 @@ echo -e "  / __ \/ /_(_) /__     _________  ____  / __(_)___ _"
 echo -e " / / / / __/ / / _ \   / ___/ __ \/ __ \/ /_/ / __ `/"
 echo -e "/ /_/ / /_/ / /  __/  / /__/ /_/ / / / / __/ / /_/ / "
 echo -e "\___\_\__/_/_/\___/   \___/\____/_/ /_/_/ /_/\__, /  "
-                                            /____/   "
+echo -e "                                            /____/   "
 
 curl -O https://github.com/iWas-Coder/iQtile/raw/main/dotfiles/qtile_config.zip
 unzip qtile_config.zip && rm qtile_config.zip
