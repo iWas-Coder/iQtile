2c6e1834f3623280b59de4ec3ce98c955546a7cb
Wasym Atieh Alonso <72618577+iWas-Coder@users.noreply.github.com>
2021-11-26 13:11:01 +0100

    Create qtile_config.sh

---
+++ scripts/qtile_config.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo -e "   ____  __  _ __                         _____      "
+echo -e "  / __ \/ /_(_) /__     _________  ____  / __(_)___ _"
+echo -e " / / / / __/ / / _ \   / ___/ __ \/ __ \/ /_/ / __ `/"
+echo -e "/ /_/ / /_/ / /  __/  / /__/ /_/ / / / / __/ / /_/ / "
+echo -e "\___\_\__/_/_/\___/   \___/\____/_/ /_/_/ /_/\__, /  "
+                                            /____/   "
+
+curl -O https://github.com/iWas-Coder/iQtile/raw/main/dotfiles/qtile_config.zip
+unzip qtile_config.zip && rm qtile_config.zip
+mv qtile_config/qtile/ ~/.config/
+
+echo "DONE! Qtile is now configured."
